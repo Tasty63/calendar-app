edbaf892a58619ffe4a4115f93f2c100161b0610
Tasty <sapegin.petya@yandex.ru>
2022-04-19 13:47:11 +0400

    feat: make changing calendar months

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,41 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { months, weekDays } from '../config/constants';
 import './Calendar.scss';
 
 function Calendar() {
-  const getDaysInMonth = () => {
-    const date = new Date();
+  const [currentDate, setDate] = useState(new Date());
+  const [daysMatrix, setDays] = useState<Number[]>([]);
 
-    const month = date.getMonth();
-    const year = date.getFullYear();
-
-    const daysInMonth = new Date(year, month + 1, 0).getDate();
-    return new Array(daysInMonth).fill(0);
+  const setNextMonth = () => {
+    setDate((prevDate) => new Date(prevDate.getFullYear(), prevDate.getMonth() + 1));
   };
 
-  const days = getDaysInMonth();
-  console.log(days);
+  const setPrevMonth = () => {
+    setDate((prevDate) => new Date(prevDate.getFullYear(), prevDate.getMonth() - 1));
+  };
+
+  const getDaysInMonth = () => {
+    const day = currentDate.getDate();
+    const month = currentDate.getMonth();
+    const year = currentDate.getFullYear();
+
+    const firstWeekDayNumber = new Date(year, month, 1).getDay();
+
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    return new Array(firstWeekDayNumber + daysInMonth).fill(null, 0, firstWeekDayNumber).fill(0, firstWeekDayNumber);
+  };
+
+  useEffect(() => {
+    setDays(getDaysInMonth());
+  }, [currentDate]);
 
   return (
     <div className="calendar">
       <div className="calendar__header">
-        <div className="button-month">{`<`}</div>
-        <div className="calendar__current-month">April 2022</div>
-        <div className="button-month">{`>`}</div>
+        <div className="button-month" onClick={setPrevMonth}>{`<`}</div>
+        <div className="calendar__current-month">{`${
+          months[currentDate.getMonth()]
+        } ${currentDate.getFullYear()}`}</div>
+        <div className="button-month" onClick={setNextMonth}>{`>`}</div>
       </div>
       <div className="calendar__body">
         <div className="calendar__weekdays">
-          <div className="calendar__weekday">Sunday</div>
-          <div className="calendar__weekday">Monday</div>
-          <div className="calendar__weekday">Tuesday</div>
-          <div className="calendar__weekday">Wednesday</div>
-          <div className="calendar__weekday">Thursday</div>
-          <div className="calendar__weekday">Friday</div>
-          <div className="calendar__weekday">Saturday</div>
+          {weekDays.map((weekDay) => (
+            <div className="calendar__weekday">{weekDay}</div>
+          ))}
         </div>
         <div className="calendar__days">
-          {days.map((day, index) => (
-            <div className="calendar__day">{index + 1}</div>
-          ))}
+          {daysMatrix.map((day, index) => {
+            return day === null ? (
+              <div key={index} className="calendar__empty"></div>
+            ) : (
+              <div key={index} className="calendar__day">
+                {index - daysMatrix.filter((day) => day === null).length + 1}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
---
+++ src/components/config/constants.ts
@@ -0,0 +1,16 @@
+export const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'Octember',
+  'November',
+  'December',
+];
+
+export const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
