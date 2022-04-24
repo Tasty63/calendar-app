a2ace52084a7ba04e1cbe5f7ca2369e99706d1d9
Tasty <sapegin.petya@yandex.ru>
2022-04-24 19:49:48 +0400

    refactor: update index.tsx due to React 18.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
